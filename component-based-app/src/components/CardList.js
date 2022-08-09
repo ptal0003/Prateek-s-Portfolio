4e6540b4e0d757d5176d8fac5d9f2ba169891dc3
Prateek Talukdar <ptal0003@student.monash.edu>
2022-08-09 15:48:36 +1000

    Cards are updated through firestore now

--- component-based-app/src/components/CardList.js
+++ component-based-app/src/components/CardList.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "./CardContainer"
 import "../css/CardList.css"
 import db from "../firebase-config";
-import {collection,getDocs} from "firebase/firestore";
-const CardList = ({questions}) =>
+import { useState } from "react";
+import {collection,doc,getDoc,getDocs} from "firebase/firestore";
+const CardList = () =>
 {
+    const [questions,setQuestions] = useState([])
+    const questionsCollectionRef = collection(db,"questions")
+    useEffect(() => {
+        const getQuestions = async () => {
+            const data = await getDocs(questionsCollectionRef);
+            setQuestions(data.docs.map((doc) => ({...doc.data()})))
+        }
+        getQuestions()
+    })
     return(
         <div>
             <div className="card-list-container-div">
